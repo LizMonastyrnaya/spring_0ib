570edf527747092bdaf06a5c16217cd185fb0db5
Elizabeth <liza.monastyrnaya@mail.ru>
2023-02-07 10:10:57 +1000

    0ib
    Monastyrnaya 0ib

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,13 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring!";
+        Date a= new Date();
+        return "Монастырная Елизавета Игоревна " + a.toString();
+
     }
+
 
 }
